02c6e8501e896f0ea88abf4af4b1f3f6c38b877b
Christian Aistleitner <christian@quelltextlich.at>
2015-03-27 10:49:57 +0100

    Put each repo in function in setup script
    Change-Id: Iabf605ebb5e1440af9d4dd85eda3c4879f936d76

--- setup.sh
+++ setup.sh
@@ -4,21 +4,38 @@
 source "$(dirname "$0")/common.inc"
 #---------------------------------------------------------------------
 
-section "Setting up Watchman"
-run_git clone https://github.com/facebook/watchman.git
-cd watchman
-./autogen.sh
-./configure
-make
-cd "$SCRIPT_DIR_ABS"
+setup_watchman() {
+    section "Setting up Watchman"
+    run_git clone https://github.com/facebook/watchman.git
+    cd watchman
+    ./autogen.sh
+    ./configure
+    make
+    cd "$SCRIPT_DIR_ABS"
+}
+
+setup_buck() {
+    section "Setting up Buck"
+    git clone https://gerrit.googlesource.com/buck
+    cd buck
+    ant
+    cd "$SCRIPT_DIR_ABS"
+}
+
+setup_repo() {
+    local REPO="$1"
+    section "Setting up $REPO"
+    mkdir -p "$(dirname "$REPO")"
+    cd "$(dirname "$REPO")"
+    git clone "https://gerrit.googlesource.com/$REPO"
+    cd "$(basename "$REPO")"
+    git checkout "$BRANCH"
+    cd "$SCRIPT_DIR_ABS"
+}
 
 
-section "Setting up Buck"
-git clone https://gerrit.googlesource.com/buck
-cd buck
-ant
-cd "$SCRIPT_DIR_ABS"
-
+setup_watchman
+setup_buck
 for REPO in \
     gerrit \
     plugins/its-base \
@@ -29,11 +46,5 @@ for REPO in \
     plugins/its-storyboard \
 
 do
-    section "Setting up $REPO"
-    mkdir -p "$(dirname "$REPO")"
-    cd "$(dirname "$REPO")"
-    git clone "https://gerrit.googlesource.com/$REPO"
-    cd "$(basename "$REPO")"
-    git checkout "$BRANCH"
-    cd "$SCRIPT_DIR_ABS"
+    setup_repo "$REPO"
 done
