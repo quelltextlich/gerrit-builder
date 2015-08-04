eb61e1df1c2df82b5328f3b163c70ef05a4303d6
Christian Aistleitner <christian@quelltextlich.at>
2015-08-04 14:32:43 +0200

    Use separate variables for ssh and http host of test site
    This separation will help us some way down the road, if we do more
    sophisticated testing/tunnelling.  As the separation is still cheap
    right now (TEST_SITE_HOST is not used too much right now), we rather
    do it now than after a year when TEST_SITE_HOST sees more use.
    
    Change-Id: I17f0cbf81aac0cccdfcc40868861910acca8cdb5

--- ssh_test-site.sh
+++ ssh_test-site.sh
@@ -12,7 +12,7 @@ ssh \
     -o "UserKnownHostsFile $TEST_SITE_USER_CREDENTIALS_DIR_ABS/$USER_NAME/known_hosts" \
     -o "StrictHostKeyChecking no" \
     -p "$TEST_SITE_SSH_PORT" \
-    "$USER_NAME@$TEST_SITE_HOST" \
+    "$USER_NAME@$TEST_SITE_SSH_HOST" \
     "$@"
 
 finalize
